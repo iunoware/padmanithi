5ec7699a1d88c497b575b47cab2225a447968d4d
Praveen123-png <praveenkumar814444@gmail.com>
2025-08-13 12:08:46 +0530

    contact 100% complete, service page 4 complete

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let calendar = new FullCalendar.Calendar(calendarNew, {
     initialView: "dayGridMonth",
     validRange: {
-      start: new Date()
+      start: new Date(),
     },
     dateClick: function (info) {
       // alert("You clicked on: " + info.dateStr);
@@ -44,7 +44,7 @@ function closePopup() {
   document.getElementById("phone").value = "";
 }
 
-// for message
+// for calender
 const messageBtn = document.getElementById("sendMessageBtn");
 const myNum = "";
 let url;
@@ -62,5 +62,43 @@ messageBtn.addEventListener("click", (event) => {
     window.open(url);
   } else {
     document.querySelector(".warning-message").style.display = "block";
+  }
+});
+
+// for the form submit
+let submitForm = document.getElementById("submit-form");
+// use the 'myNum' here:
+
+submitForm.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let fName = document.getElementById("fName").value.trim();
+  let lName = document.getElementById("lName").value.trim();
+  let emailForm = document.getElementById("email-form").value.trim();
+  let phoneNumForm = document.getElementById("phoneNum-form").value.trim();
+  let companyName = document.getElementById("companyName").value.trim();
+  let location = document.getElementById("location").value.trim();
+  let messageForm = document.getElementById("message-form").value.trim();
+
+  if (
+    fName &&
+    lName &&
+    emailForm &&
+    phoneNumForm &&
+    location &&
+    companyName &&
+    messageForm
+  ) {
+    const messageNew = `Hi, I'm ${fName} ${lName},\nThese are my details \n• Mobile: ${phoneNumForm}.\n• Email: ${emailForm}\n• location: ${location}\n• Company name: ${companyName}\n• Message: ${messageForm} \n\nThank you!`;
+    const enCoded = encodeURIComponent(messageNew);
+    url = `https://wa.me/${myNum}?text=${enCoded}`;
+    window.open(url);
+  } else if (fName && lName && emailForm && phoneNumForm && location && companyName) {
+    const messageNew = `Hi, I'm ${fName} ${lName},\nThese are my details \n• Mobile: ${phoneNumForm}.\n• Email: ${emailForm}\n• location: ${location}\n• Company name: ${companyName} \n\nThank you!`;
+    const enCoded = encodeURIComponent(messageNew);
+    url = `https://wa.me/${myNum}?text=${enCoded}`;
+    window.open(url);
+  } else {
+    document.querySelector(".warning-message-form").style.display = "block";
   }
 });
